a8b81a3f7d1accc31ac9e35ded45b47944cb8427
Maradana, Vishal <vmaradana3@gatech.edu>
2022-10-24 13:30:10 -0400

    Add files via upload

--- app/src/main/java/com/example/team27pac_man/MainActivity.java
+++ app/src/main/java/com/example/team27pac_man/MainActivity.java
@@ -1,113 +1,34 @@
 package com.example.team27pac_man;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.graphics.Color;
-import android.media.Image;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.initial_configuration);
-
-        Spinner spinner = (Spinner) findViewById(R.id.difficultySpinner);
-        Spinner spriteSpinner = (Spinner) findViewById(R.id.spriteSpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.difficultiesArray,
-                android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.spritesArray,
-                android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spriteSpinner.setAdapter(adapter2);
-        spriteSpinner.setOnItemSelectedListener(this);
-
-        TextView errorMessage = (TextView) findViewById(R.id.errorTextView);
-
-        Button contButton = (Button) findViewById(R.id.startButton);
-        Button startButton = (Button) findViewById(R.id.button);
-        startButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+        Button start = (Button) (findViewById(R.id.start_button));
+        Button quit = (Button) (findViewById(R.id.quit_button));
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editName = (EditText) findViewById(R.id.nameEditText);
-                String playerName;
-                if (editName.getText().toString().contains(" ")
-                        || editName.getText().toString().isEmpty()
-                        || editName.getText().toString().trim().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please enter a valid name", Toast.LENGTH_LONG).show();
-
-                    errorMessage.setTextColor(Color.RED);
-                    errorMessage.setVisibility(View.VISIBLE);
-                    contButton.setVisibility(View.GONE);
-                    playerName = "Invalid input";
-                } else {
-                    errorMessage.setVisibility(View.GONE);
-                    playerName = editName.getText().toString();
-
-                    contButton.setVisibility(View.VISIBLE);
-                    contButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            TextView difficultyText = (TextView) findViewById(R.id.difficultyTextView);
-                            String str = ((difficultyText.getText().toString()).split(" "))[1];
-                            Intent intent = new Intent(MainActivity.this, game.class);
-                            intent.putExtra("difficulty", str);
-                            startActivity(intent);
-
-                        }
-                    });
-                }
-                TextView nameTextView = (TextView) findViewById(R.id.nameTextView);
-                nameTextView.setText("Player: " + playerName);
+                setContentView(R.layout.initial_configuration);
+                Intent intent = new Intent(MainActivity.this, configure.class);
+                startActivity(intent);
             }
         });
-
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-
-        switch (parent.getId()) {
-            case R.id.difficultySpinner: {
-                String text = parent.getItemAtPosition(pos).toString();
-                Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
-                TextView difficultyText = (TextView) findViewById(R.id.difficultyTextView);
-                difficultyText.setText("Difficulty: " + text);
+        quit.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
-            case R.id.spriteSpinner: {
-                ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                String text = parent.getItemAtPosition(pos).toString();
-                if (text.equals("mrPacMan")) {
-                    imageView.setImageResource(R.drawable.mr_pacman);
-                } else if (text.equals("msPacMan")) {
-                    imageView.setImageResource(R.drawable.ms_pacman);
-                } else {
-                    imageView.setImageResource(R.drawable.aware_pacman);
-                }
-            }
-        }
-
+        });
     }
 
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
 }
