47caa0fd53ada3e1c0986d3fb6503876085094fc
apatel768 <apatel768@gatech.edu>
2022-10-30 06:15:47 -0400

    Working on the layout for maze and sprites

--- app/src/main/java/com/example/team27pac_man/Maze.java
+++ app/src/main/java/com/example/team27pac_man/Maze.java
@@ -3,8 +3,10 @@ package com.example.team27pac_man;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.content.Context;
 import android.view.View;
 import android.widget.TableLayout;
@@ -18,6 +20,8 @@ public class Maze extends AppCompatActivity implements AdapterView.OnItemSelecte
     private int totalColumns;
     private int blockLength;
     private Bitmap[][] layout;
+    private ImageView imageView;
+//    private int[][] layoutIds;
 //    int screenHeight;
 
     @Override
@@ -32,11 +36,11 @@ public class Maze extends AppCompatActivity implements AdapterView.OnItemSelecte
         lives.setText("Lives: "+ configure.getLives());
         difficulty.setText("Difficulty: " + configure.getDifficulty());
         //score.setText();
-        initializeSize(); //creates the maze
+//        initializeSize(); //creates the maze
     }
 
     public void initializeSize(){
-        createNewLayout();
+//        createNewLayout();
         DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
         screenWidth = displayMetrics.widthPixels;// width of screen
         totalColumns = 16; // 408 pixels into 16 columns- column size = 25
@@ -45,14 +49,15 @@ public class Maze extends AppCompatActivity implements AdapterView.OnItemSelecte
         //height and width of an image
         blockLength = screenWidth/totalColumns;
 //        int blockHeight = screenHeight/totalRows;
-        initializeBitMap(blockLength);
+//        initializeBitMap(blockLength);
+        createNewLayout();
 //
 //        };
     }
-    private void initializeBitMap(int blockSize){
-        int spriteSize = (blockSize/9) * 9; // multiple of 9
-        int ghostBoxSize = blockSize * 4; // for 4 ghosts
-    }
+//    private void initializeBitMap(int blockSize){
+//        int spriteSize = (blockSize/9) * 9; // multiple of 9
+//        int ghostBoxSize = blockSize * 4; // for 4 ghosts
+//    }
 
     //layout key:
     // 0: random wall
@@ -65,7 +70,7 @@ public class Maze extends AppCompatActivity implements AdapterView.OnItemSelecte
     // 7: vetical long wall(for border)
     // 8: power pellets - 5
     private void createNewLayout(){
-        int[][] layoutIds = {
+        int [][] layoutIds = {
                 {6, 5, 6, 5, 6, 5, 6, 5, 6, 5, 6, 5, 6, 5, 6, 5},
                 {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
                 {7, 5, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 7, 5},
@@ -86,8 +91,43 @@ public class Maze extends AppCompatActivity implements AdapterView.OnItemSelecte
                 {5,5,1,1,1,1,1,1,8,1,1,1,1,1,5,5},
                 {6,5,6,5,6,5,5,5,5,5,6,5,6,5,7,5},
                 {5,5,5,5,5,5,5,2,5,5,5,5,5,5,5,5}
-
         };
+        int spriteSize = (blockLength/9) * 9; // multiple of 9
+        int wallSize = blockLength * 4; // for 4 ghosts
+        int powerPelletSize = 8;
+        int pelletSize = 4;
+        layout = new Bitmap[20][16]; //20 rows and 16 columns
+        for(int i = 0; i < layoutIds.length; i++){
+            for(int j = 0; j < layoutIds[0].length; j++){
+                if(layoutIds[i][j] == 0){
+                    //make this random using enums- just using short wall for now
+                    layout[i][j] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.hshort_wall), wallSize, wallSize, false);
+                } else if(layoutIds[i][j] == 1){
+                    layout[i][j] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pellet), pelletSize, pelletSize, false);
+                } else if(layoutIds[i][j] == 2){
+                    //MAKE SURE THE RIGHT PACMAN IS SELECTED- aware pacman for default
+                    layout[i][j] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.aware_pacman), spriteSize, spriteSize, false);
+                } else if(layoutIds[i][j] == 3){
+                    //MAKE SURE ALL 4 GHOSTS are DISPLAYED - choosing 1 ghost for now
+                    layout[i][j] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pink_modified), spriteSize, spriteSize, false);
+                } else if(layoutIds[i][j] == 4){
+                    layout[i][j] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ghostbox), wallSize, wallSize, false);
+
+                } else if (layoutIds[i][j] == 5){
+                    continue;
+                } else if(layoutIds[i][j] == 6){
+                    layout[i][j] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.hlong_wall), wallSize, wallSize, false);
+
+                } else if(layoutIds[i][j] == 7){
+                    layout[i][j] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.vlong_wall), wallSize, wallSize, false);
+
+                } else if(layoutIds[i][j] == 8){
+                    layout[i][j] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.power_pellet), powerPelletSize, powerPelletSize, false);
+                }
+                imageView.setImageBitmap(layout[i][j]);
+            }
+        }
+
     }
 
     @Override
