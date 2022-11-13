a9be39b4ccbf62bebb10a55246d688ee6e436325
cscales8 <cscales8@gatech.edu>
2022-11-12 19:49:52 -0500

    Added End Screen

---
+++ app/src/main/java/com/example/team27pac_man/End.java
@@ -0,0 +1,32 @@
+package com.example.team27pac_man;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class End extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.end);
+        Button restart = (Button) findViewById(R.id.restart_button);
+        Button quit = (Button) findViewById(R.id.end_button);
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.initial_configuration);
+                Intent intent = new Intent(End.this, configure.class);
+                startActivity(intent);
+            }
+        });
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
